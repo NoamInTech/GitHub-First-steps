a4fdfb4a692ac0647e4eab4234f7cc89021fcf86
NoamInTech <noemie.pivin@gmail.com>
2023-01-04 17:05:25 +0000

    Update weather icon

--- src/script.js
+++ src/script.js
@@ -23,8 +23,12 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-//Week 5 - APIs
-// Main Feature
+function searchCity(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
 function showTemperature(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -35,14 +39,15 @@ function showTemperature(response) {
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.weather[0].description;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-function searchCity(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemperature);
-}
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#searched-city").value;
